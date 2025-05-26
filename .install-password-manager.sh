67383fdd9ae126d0e65b2ef08d6163aedc9ffb64
5739n4 <8215071+5739n4@users.noreply.github.com>
2025-05-25 21:48:51 -0400

    Ensure Homebrew is installed before installing dependencies in scripts

--- .install-password-manager.sh
+++ .install-password-manager.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
+command -v brew >/dev/null 2>&1 || \
+  (echo '🍺  Installing Homebrew' && /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)")
+
 # exit immediately if apw is already in $PATH
 type apw >/dev/null 2>&1 && exit
 
-
-
 case "$(uname -s)" in
 Darwin)
+    echo '🔑  Installing apw' 
     eval "$(/opt/homebrew/bin/brew shellenv 2>/dev/null || /usr/local/bin/brew shellenv)"
     brew install bendews/homebrew-tap/apw && brew services restart apw
     apw auth
