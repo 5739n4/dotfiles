439672537bda85f88c4fc56d74a8cb6ad1694d89
databandit <8215071+5739n4@users.noreply.github.com>
2025-12-09 11:24:38 -0500

    Enhance install script to wait for Xcode CLI tools installation completion

--- install.sh
+++ install.sh
@@ -2,7 +2,12 @@
 set -e
 
 # 1. Xcode CLI tools (needed for git, brew)
+# skips to next  step after invoking popup to install CLI tools, i want it to wait until setup is done
 xcode-select -p &>/dev/null || xcode-select --install
+while ! xcode-select -p &>/dev/null; do
+  echo "Waiting for Xcode CLI tools to be installed..."
+  sleep 1
+done
 
 # 2. Homebrew (idempotent)
 if ! command -v brew >/dev/null; then
