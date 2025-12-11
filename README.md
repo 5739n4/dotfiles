28b8a63d56961e4984a7d4e32f9ee68f276abca6
databandit <8215071+5739n4@users.noreply.github.com>
2025-12-10 22:17:35 -0500

    Add new Homebrew packages and update installation instructions in README
    - Added 'chezmoi', 'fnm', and 'git-town' to Brewfile for package management.
    - Updated dot_zprofile to ensure Homebrew environment is set correctly.
    - Enhanced README with detailed installation steps for Homebrew and chezmoi.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-`sh -c "$(curl -fsLS get.chezmoi.io/lb)" -- init --apply $GITHUB_USERNAME`
+## Installation steps
+
+1. `/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"`
+2. `eval "$(/opt/homebrew/bin/brew shellenv)"`
+3. `brew install chezmoi && chezmoi init --apply $GITHUB_USERNAME`
--- run_onchange_before_00-install_core_apps.sh
+++ run_onchange_before_00-install_core_apps.sh
@@ -1,35 +1,8 @@
 #!/usr/bin/env zsh
 set -e
 
-# 1. Xcode CLI tools (needed for git, brew)
-# skips to next  step after invoking popup to install CLI tools, i want it to wait until setup is done
-xcode-select -p &>/dev/null || xcode-select --install
-# Animated spinner while waiting for Xcode CLI tools installation
-spinner='|/-\'
-i=0
-echo -n "Waiting for Xcode CLI tools to be installed... "
-while ! xcode-select -p &>/dev/null; do
-  printf "\rWaiting for Xcode CLI tools to be installed... %s" "${spinner[$((i++ % ${#spinner}))]}"
-  sleep 5
-done
-printf "\rWaiting for Xcode CLI tools to be installed... done!\n"
+brew bundle
 
-# 2. Homebrew (idempotent)
-if ! command -v brew >/dev/null; then
-  sudo -v
-  NONINTERACTIVE=1 /bin/bash -c \
-    "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-fi
-
-[ -f /opt/homebrew/bin/brew ] && eval "$(/opt/homebrew/bin/brew shellenv)"
-[ -f /usr/local/bin/brew ] && eval "$(/usr/local/bin/brew shellenv)"
-
-# 3. chezmoi
-brew install chezmoi
-
-# 5. Open Apple ID preferences
+# Open Apple ID preferences
 open "x-apple.systempreferences:com.apple.preferences.internetaccounts"
-
-# 6. Done
-echo "🎉 Setup complete! Your dotfiles are now managed by chezmoi."
 
